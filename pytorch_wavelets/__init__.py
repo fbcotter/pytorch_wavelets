fd0151b4d09d1de4b5bc7f331cc1cfee93ec6c7e
Fergal Cotter <fbcotter90@gmail.com>
2018-09-19 11:08:08 +0100

    Updated package init file

--- pytorch_wavelets/__init__.py
+++ pytorch_wavelets/__init__.py
@@ -1,15 +1,19 @@
-import os
-import sys
-
 __all__ = [
     '__version__',
-
-    #  'Transform1d',
     'DTCWTForward',
     'DTCWTInverse',
-    #  'Transform3d',
+    'DWTForward',
+    'DWTInverse',
+    'DTCWT',
+    'IDTCWT',
+    'DWT',
+    'IDWT',
 ]
 
 from pytorch_wavelets._version import __version__
 from pytorch_wavelets.dtcwt.transform2d import DTCWTForward, DTCWTInverse
 from pytorch_wavelets.dwt.transform2d import DWTForward, DWTInverse
+DTCWT = DTCWTForward
+IDTCWT = DTCWTInverse
+DWT = DWTForward
+IDWT = DWTInverse
