70a04a01ed0dd135784ba3a5ac419e6c78131098
Fergal Cotter <fbcotter90@gmail.com>
2019-05-08 11:43:16 +0100

    Fix dtcwt grad test

--- tests/test_dtcwt_grad.py
+++ tests/test_dtcwt_grad.py
@@ -3,6 +3,7 @@ import pytest
 from torch.autograd import gradcheck
 from pytorch_wavelets.dtcwt.transform2d import DTCWTForward, DTCWTInverse
 import pytorch_wavelets.dtcwt.transform_funcs as tf
+from pytorch_wavelets.dwt.lowlevel import mode_to_int
 import py3nvml
 from contextlib import contextmanager
 ATOL = 1e-4
@@ -25,6 +26,10 @@ def set_double_precision():
 
 
 def setup():
+    global mode, o_dim, ri_dim
+    mode = mode_to_int('symmetric')
+    o_dim = 2
+    ri_dim = -1
     py3nvml.grab_gpus(1, gpu_fraction=0.5)
 
 
@@ -34,7 +39,7 @@ def test_fwd_j1(skip_hps):
         x = torch.randn(1,3,16,16, device=dev, requires_grad=True)
         xfm = DTCWTForward(J=2).to(dev)
 
-    input = (x, xfm.h0o, xfm.h1o, skip_hps, 2, -1)
+    input = (x, xfm.h0o, xfm.h1o, skip_hps, o_dim, ri_dim, mode)
     gradcheck(tf.FWD_J1.apply, input, eps=1e-3, atol=ATOL)
 
 
@@ -43,7 +48,7 @@ def test_fwd_j2(skip_hps):
     with set_double_precision():
         x = torch.randn(1,3,16,16, device=dev, requires_grad=True)
         xfm = DTCWTForward(J=2).to(dev)
-    input = (x, xfm.h0a, xfm.h1a, xfm.h0b, xfm.h1b, skip_hps, 2, -1)
+    input = (x, xfm.h0a, xfm.h1a, xfm.h0b, xfm.h1b, skip_hps, o_dim, ri_dim, mode)
     gradcheck(tf.FWD_J2PLUS.apply, input, eps=1e-3, atol=ATOL)
 
 
@@ -52,7 +57,7 @@ def test_inv_j1():
         low = torch.randn(1,3,16,16, device=dev, requires_grad=True)
         high = torch.randn(1,3,6,8,8,2, device=dev, requires_grad=True)
         ifm = DTCWTInverse().to(dev)
-    input = (low, high, ifm.g0o, ifm.g1o, 2, -1)
+    input = (low, high, ifm.g0o, ifm.g1o, o_dim, ri_dim, mode)
     gradcheck(tf.INV_J1.apply, input, eps=1e-3, atol=ATOL)
 
 
@@ -61,5 +66,5 @@ def test_inv_j2():
         low = torch.randn(1,3,16,16, device=dev, requires_grad=True)
         high = torch.randn(1,3,6,8,8,2, device=dev, requires_grad=True)
         ifm = DTCWTInverse().to(dev)
-    input = (low, high, ifm.g0a, ifm.g1a, ifm.g0b, ifm.g1b, 2, -1)
+    input = (low, high, ifm.g0a, ifm.g1a, ifm.g0b, ifm.g1b, o_dim, ri_dim, mode)
     gradcheck(tf.INV_J2PLUS.apply, input, eps=1e-3, atol=ATOL)
